f3f45a460320dbf3474531cad657ca3f8a714c14
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2021-12-26 17:04:07 +0330

    fit:(add navigation component)

--- app/src/main/java/com/rezapour/cazootask/ui/MainActivity.kt
+++ app/src/main/java/com/rezapour/cazootask/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.rezapour.cazootask.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rezapour.cazootask.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
