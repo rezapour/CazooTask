f44afe24694463396ce3eccf5620789a3c26403c
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2021-12-26 17:20:19 +0330

    fit:(turn on viewBiding)

--- app/src/main/java/com/rezapour/cazootask/ui/CarListFragment.kt
+++ app/src/main/java/com/rezapour/cazootask/ui/CarListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.rezapour.cazootask.R
+import com.rezapour.cazootask.databinding.FragmentCarListBinding
 import com.rezapour.cazootask.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,12 +15,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class CarListFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
+    private var _binding: FragmentCarListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car_list, container, false)
+        _binding = FragmentCarListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 }
--- app/src/main/java/com/rezapour/cazootask/ui/MainActivity.kt
+++ app/src/main/java/com/rezapour/cazootask/ui/MainActivity.kt
@@ -3,12 +3,18 @@ package com.rezapour.cazootask.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rezapour.cazootask.R
+import com.rezapour.cazootask.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
