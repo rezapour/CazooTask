f1556ddb06adb57f25ef7e5775c0b1138807500a
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2021-12-30 12:54:27 +0330

    fit:(Splash screen->add splash screen)

---
+++ app/src/main/java/com/rezapour/cazootask/ui/SplashScreanFragment.kt
@@ -0,0 +1,41 @@
+package com.rezapour.cazootask.ui
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.rezapour.cazootask.R
+import com.rezapour.cazootask.databinding.FragmentSplashScreanBinding
+
+class SplashScreanFragment : Fragment() {
+
+    private var _binding: FragmentSplashScreanBinding? = null
+    private val binding get() = _binding!!
+    private var navControler: NavController? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashScreanBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navControler = Navigation.findNavController(view)
+        nextFragment()
+    }
+
+    private fun nextFragment() {
+        Handler().postDelayed({
+            navControler!!.navigate(R.id.action_splashScreanFragment_to_carListFragment)
+        }, 2000)
+    }
+
+}
